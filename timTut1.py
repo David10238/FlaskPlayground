c8344eec32ca4f75f6abfb6039817ef917d1afd7
David Lukens <davidqcy02@gmail.com>
2021-03-04 19:58:42 -0600

    feeding variables into redirect

--- timTut1.py
+++ timTut1.py
@@ -1,19 +1,19 @@
 
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # home route
 def home():
     return "Welcome to the home page<h1>HELLO<h1>"
 
-@app.route("/<name>")
+@app.route("/<name>") # passing parameter to method
 def user(name):
     return f"Hello {name} git is synched with VSCode"
 
-@app.route("/admin")
+@app.route("/admin/") # / at end allows having / at end
 def admin():
-    return redirect(url_for("home"))
+    return redirect(url_for("user", name="Admin")) #redirecting to another function
 
 if __name__ == "__main__":
     app.run(debug=True)
